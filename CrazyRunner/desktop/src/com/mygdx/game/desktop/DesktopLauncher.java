d7d44df5ef5a9dfdae4f479eaaf7c6903611eb18
peraa0837 <peraa0837@HRH-IC0027324.wrdsb.ca>
2018-01-10 09:21:51 -0500

    Initial commit

--- CrazyRunner/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ CrazyRunner/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,10 +2,14 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
 import com.mygdx.game.CrazyRunner;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+            Settings settings = new Settings();
+            settings.maxHeight = 1600;
+            settings.maxWidth = 900;
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new CrazyRunner(), config);
 	}
