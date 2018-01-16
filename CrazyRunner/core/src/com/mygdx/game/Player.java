6ee0a466352eb28af19a03617a12fc39c6fcfc91
sheza <sheza@LAPTOP-61QO2QGF>
2018-01-16 10:21:26 -0500

    player edit

--- CrazyRunner/core/src/com/mygdx/game/Player.java
+++ CrazyRunner/core/src/com/mygdx/game/Player.java
@@ -1,14 +1,71 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 /**
  *
- * @author sheza
+ * @author khans4349
  */
 public class Player {
+    // instance variables for location of player
+    // x coordinate
+    private int x;
+    // y coordinate
+    private int y;
+    // spriteBatch
+    private SpriteBatch batch;
+    // texture 
+    private Texture personImg;
+    // screen boundaries
+    // nvr mind ignore this (for collision detection only)
+    private boolean topBoundary;
+    private boolean bottemBoundary;
+    private boolean leftBoundary;
+    private boolean rightBoundary;
     
+    public void create(){
+        batch = new SpriteBatch();
+        personImg = new Texture("person.png");
+    }
+    
+    /**
+     *
+     * @param player
+     */
+    public void render(SpriteBatch player){
+        // keyboard input
+        // if player moves up
+        if(Gdx.input.isKeyPressed(Input.Keys.UP)){
+            y = y + 15;
+        }
+        // if player moves down
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+            y = y - 15;
+        }
+        // if player moves right
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            x = x + 15;
+        }
+        // if player moves left
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT))  {
+            x = x - 15;
+        }
+        
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(personImg, 0, 0, x, y);
+        batch.end();
+        
+    }
 }
+
+
